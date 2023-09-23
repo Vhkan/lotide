2f7d80983459081814bd998ef03457a42ee20fab
Vhkan <vhkan@yahoo.com>
2023-09-23 11:13:25 -0600

    Adding a refactored code for assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,21 +1,3 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`${actual} === ${expected}`);
-  } else {
-    console.log(`${actual} !== ${expected}`);
-  }
-};
-
-const eqArrays = function (arr1, arr2) {
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    } else {
-      return true;
-    }
-  }
-};
-
 const assertArraysEqual = function (arr1, arr2) {
   const result = eqArrays(arr1, arr2);
   if (result) {
@@ -24,3 +6,5 @@ const assertArraysEqual = function (arr1, arr2) {
     console.log("Arrays are not equal.");
   }
 }
+
+module.exports = assertArraysEqual;
