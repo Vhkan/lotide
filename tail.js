a9d0a27b3dbe893400cd723afb2b274ae52fdc41
Vhkan <vhkan@yahoo.com>
2023-09-23 10:06:24 -0600

    Adding testing files for tail file

--- tail.js
+++ tail.js
@@ -1,19 +1,8 @@
+const assertEqual = require('./assertEqual');
+
 const tail = function (arr) { 
   const newArray = arr.slice(1);
   return newArray;
 };
 
-const assertEqual = function (actual, expected) {
-
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`)
-  } else if (actual !== expected) {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
-}
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-// const result = tail(["Hello", "Lighthouse", "Labs"]);
-// assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
+module.exports = tail;
